ed3bbfa76e9da52ce26fc1b0572cb25ecd2da185
КрахМастер КрахМастер <mokrushin.sr@gmail.com>
2023-02-02 21:20:07 +0500

    Добавьте файлы проекта.

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Введите кол-во строк:");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов");
+int col = int.Parse(Console.ReadLine());
+
+int[,] array = new int[row, col];
+
+Random r = new Random();
+Console.WriteLine("Вывод полученной матрицы:");
+
+int sum = 0;
+
+for (int i = 0; i < row; i++)
+{
+    
+    for(int j = 0; j < col; j++)
+    {
+        array[i, j] = r.Next(100);
+        Console.Write($"{array[i, j]} ");
+        sum = sum + array[i, j];
+
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine($"Сумма всех элементов: {sum} ");
---
+++ Array2/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Введите кол-во строк:");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов");
+int col = int.Parse(Console.ReadLine());
+
+int[,] array = new int[row, col];
+
+Random r = new Random();
+Console.WriteLine("Вывод первой полученной матрицы:");
+int sum = 0;
+
+
+for (int i = 0; i < row; i++)
+{
+
+    for (int j = 0; j < col; j++)
+    {
+        array[i, j] = r.Next(10);
+        Console.Write($"{array[i, j]} ");
+        
+    }
+    Console.WriteLine();
+
+}
+
+Random d = new Random();
+int[,] arr2d = new int[row, col];
+
+Console.WriteLine("Вывод второй полученной матрицы:");
+
+for (int i = 0; i < row; i++)
+{
+
+    for (int j = 0; j < col; j++)
+    {
+        arr2d[i, j] = d.Next(10);
+        Console.Write($"{arr2d[i, j]} ");
+       
+    }
+    Console.WriteLine();
+}
+
+
+int[,] arrSum = new int[row, col];
+Console.WriteLine("Вывод суммы массивов:");
+
+
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+        arrSum[i, j] = arr2d[i, j] + array[i, j];
+        Console.Write($"{arrSum[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
